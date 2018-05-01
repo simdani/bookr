e82f924c32f2c9d98c98b097a8a16d51c0db08f1
Simonas Daniliauskas <dranxman@gmail.com>
2018-05-01 13:56:58 +0300

    update databse config for production

--- config/database.example.js
+++
@@ -1,9 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    mongoURI: '<MONGO-DB-address>'
-  };
-} else {
-  module.exports = {
-    mongoURI: '<MONGO-DB-address>'
-  };
-}
