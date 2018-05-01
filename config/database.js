1092d130e4b6f6f345cd451d40e35c516bd341a3
Simonas Daniliauskas <dranxman@gmail.com>
2018-05-01 11:51:32 +0300

    update prod

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb://testas:testas@ds247317.mlab.com:47317/vmeasy'
+    mongoURI: 'mongodb://testas:testas@ds159509.mlab.com:59509/bookr-dev'
   };
 } else {
   module.exports = {
