faf889441323b189736e43226e1e7f918660f605
Simonas Daniliauskas <dranxman@gmail.com>
2018-05-01 13:32:56 +0300

    add project readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple book tracking and review web app
+
+Web created using:
+* Nodejs
+* Express.js framework
+* Mongo database
--- config/database.js
+++
@@ -1,9 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    mongoURI: 'mongodb://testas:testas@ds159509.mlab.com:59509/bookr-dev'
-  };
-} else {
-  module.exports = {
-    mongoURI: 'mongodb://testas:testas@ds159509.mlab.com:59509/bookr-dev'
-  };
-}
