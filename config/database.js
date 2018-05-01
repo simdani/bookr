1096980618069ce22a3da052f55f349352d56c11
Simonas Daniliauskas <dranxman@gmail.com>
2018-05-01 14:09:58 +0300

    update databse config for production #2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./database_prod');
+  module.exports = {
+    mongoURI: process.env.MONGO_URI
+  };
 } else {
   module.exports = require('./database_dev');
 }
